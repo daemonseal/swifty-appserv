3e6800472fbf0187bf546580fed3c06297b4e9c7
daemonseal <daemonseal@pm.me>
2020-02-21 15:16:01 +0700

    Update

--- app.py
+++ app.py
@@ -35,10 +35,3 @@ while True:
   time.sleep(60)
 
 mqtt_client.close()
-
-# using application manager client
-# app_client =  handler.application()
-# my_app = app_client.get()
-# print(my_app)
-# my_devices = app_client.devices()
-# print(my_devices)

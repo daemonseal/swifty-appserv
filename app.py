b94d760f8dc33757e5739d34b5d3dff84394318b
daemonseal <daemonseal@pm.me>
2020-02-24 23:46:22 +0700

    Update

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ access_key = "ttn-account-v2.K95QNh-Gfu05j6l6IkRglhgKUcFcnNiDYFbou3HOcrE"
 def uplink_callback(msg, client):
   print("Received uplink from ", msg.dev_id)
   print(msg)
+  print(msg.payload_raw)
 
 def connect_callback(res, client):
   if res:
@@ -24,13 +25,14 @@ mqtt_client.set_connect_callback(connect_callback)
 # Target device
 dev_id = "rak-node02"
 payload = {
-  "led_state": "off",
+  "test": "testing...",
   "counter": 1
 }
 
 mqtt_client.connect()
 
 while True:
+  print("Send testing msg...")
   mqtt_client.send(dev_id, payload, port=1)
   time.sleep(60)
 
